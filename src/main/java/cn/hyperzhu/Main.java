223d9297bc044d9e18d28f0c4ce975fe550a19e5
modekangkang <modekangkang@foxmail.com>
2024-08-20 17:47:55 +0800

    init project

--- src/main/java/cn/hyperzhu/Main.java
+++
@@ -1,7 +0,0 @@
-package cn.hyperzhu;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/test/java/cn/hyperzhu/pay/test/ApiTest.java
@@ -0,0 +1,7 @@
+package cn.hyperzhu.pay.test;
+
+public class ApiTest {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
